0b67b0ec099e33f3fb1ef078af588f73473c37f5
Paolo Chillari <killer.paolo@gmail.com>
2013-03-10 17:48:23 +0100

    service customer modified

--- app/public/scripts/services/customer.js
+++ app/public/scripts/services/customer.js
@@ -2,11 +2,9 @@
 
 angular.module('publicApp').
 factory('Customer', function ($resource) {
-	console.log('factory called');
 	return $resource('/users/:id', {id:'@id'},{
 		update:{
 			method: 'PUT',
-			isArray: false,
 			params: {user: '@user'}
 		}
 	});
